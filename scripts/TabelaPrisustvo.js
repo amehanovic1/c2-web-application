af6550ae833014b3404f2af47bd6ade10151c286
Adna <amehanovic2@etf.unsa.ba>
2022-11-26 00:21:00 +0100

    naziv predmeta i zaglavlje

--- scripts/TabelaPrisustvo.js
+++ scripts/TabelaPrisustvo.js
@@ -1,6 +1,26 @@
 let TabelaPrisustvo = function (divRef, podaci) {
     //privatni atributi modula
     //
+    divRef.textContent = "";
+
+    let nazivPredmeta = document.createElement("h1");
+    nazivPredmeta.textContent = podaci.predmet;
+    divRef.appendChild(nazivPredmeta);
+
+    //Zaglavlje 
+    let tekst = ["Ime i prezime", "Index", "I", "II", "III", "IV", "V", "VI", "VII", "VIII - XV"];
+    let tabela = document.createElement("table");
+    let red = document.createElement("tr");
+
+    for (let i = 0; i < tekst.length; i++) {
+        let tag = document.createElement("th");
+        let zaglavlje = document.createTextNode(tekst[i]);
+        tag.appendChild(zaglavlje);
+        red.appendChild(tag);
+    }
+    tabela.appendChild(red);
+    divRef.appendChild(tabela);
+
     //implementacija metoda
     let sljedecaSedmica = function () {
     }
