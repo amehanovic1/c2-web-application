7e0a640d27c7ef1a2062aa934a6788e0bd44ba14
Adna <amehanovic2@etf.unsa.ba>
2023-01-08 23:48:09 +0100

    dodana forma

--- index.js
+++ index.js
@@ -1,4 +1,73 @@
-const express = require('express');
-const app = express;
-app.request(express.static('public'));
+import nastavnici from './public/data/nastavnici.json' assert {type: 'json'};
+//const nastavnici = require('./public/data/nastavnici.json');
+
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+    secret: 'assdfghjkallsoekd',
+    resave: true,
+    saveUninitialized: true
+}))
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/public/html/prijava.html");
+});
+
+function nadjiNastavnika(username, password) {
+    for(let i = 0; i < nastavnici.length; i++) {
+        if(nastavnici[i].username == username && nastavnici[i].password_hash == password) {
+            return nastavnici[i];
+        }
+    }
+}
+
+app.post('/login', function(req, res) {
+    let username, password, poruka = "";
+    username = req.body.username;
+    password = req.body.password_hash;
+    /*if(req.session) {
+        username = req.body.username;
+        password = req.body.password;
+    }
+    else {
+        //nije uneseno
+    }*/
+    let nastavnik = nadjiNastavnika(username, password);
+    //provjeriti da li ima u nastavnici.json
+    if(nastavnik) {
+        //Ukoliko je ispravno loginovan korisnik u sesiju upišite username korisnika i listu predmeta na kojima je on nastavnik
+        req.session.username = username;
+        req.session.predmeti = nastavnik.predmeti;
+        //U odgovoru vratite json u formatu...
+        poruka = "Uspješna prijava";
+    }
+    else {
+        poruka = "Neuspješna prijava";
+    }
+});
+
+//za loginovanog nastavnika lista predmeta se može dobiti na ruti GET /predmeti.
+app.get('/predmeti', function(req, res) {
+
+});
+
+app.get('/predmeti.html', function(req, res) {
+    res.sendFile(__dirname + "/public/html/predmeti.html");
+});
+
+//dodati na stranicu
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+})
+
 app.listen(3000);
--- public/scripts/poziviAjax.js
+++ public/scripts/poziviAjax.js
@@ -7,19 +7,35 @@ const PoziviAjax = (()=>{
     // vraća listu predmeta za loginovanog nastavnika ili grešku da nastavnik nije loginovan
     function impl_getPredmeti(fnCallback){
     }
-    function impl_postLogin(username,password,fnCallback){
+    
+    function impl_postLogin(username, password, fnCallback){
+        username = document.getElementById("username");
+        password = document.getElementById("password");
+        var ajax = new XMLHttpRequest();
+        ajax.onreadystatechange = function() {
+        if(ajax.readyState == 4 && ajax.status == 200) {
+            var jsonRez = JSON.parse(ajax.responseText);
+            fnCallback(jsonRez.username, jsonRez.password);
+        }
+        if(ajax.readyState == 4)
+            fnCallback(ajax.statusText, null);
+        }
+        ajax.open("POST", "http://localhost:3000/login", true);
+        ajax.setRequestHeader("Content-Type", "application/json");
+        //ajax.send();
     }
+
     function impl_postLogout(fnCallback){
     }
     //prisustvo ima oblik {sedmica:N,predavanja:P,vjezbe:V}
     function impl_postPrisustvo(naziv,index,prisustvo,fnCallback){
     }
     return{
-    postLogin: impl_postLogin,
-    postLogout: impl_postLogout,
-    getPredmet: impl_getPredmet,
-    getPredmeti: impl_getPredmeti,
-    postPrisustvo: impl_postPrisustvo
+        postLogin: impl_postLogin,
+        postLogout: impl_postLogout,
+        getPredmet: impl_getPredmet,
+        getPredmeti: impl_getPredmeti,
+        postPrisustvo: impl_postPrisustvo
     };
 })();
     
