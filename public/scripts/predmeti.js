1e7efda4173b11e22885d59cac873f44c9b3b0a9
Adna <amehanovic2@etf.unsa.ba>
2023-01-17 06:35:13 +0100

    ispravke

--- public/scripts/predmeti.js
+++ public/scripts/predmeti.js
@@ -1,43 +1,3 @@
-let prisustvo = {
-    "studenti": [{
-    "ime": "Neko Nekic",
-    "index": 12345
-    },
-    {
-    "ime": "Drugi Neko",
-    "index": 12346
-    }
-    ],
-    "prisustva": [{
-    "sedmica": 1,
-    "predavanja": 2,
-    "vjezbe": 1,
-    "index": 12345
-    },
-    {
-    "sedmica": 1,
-    "predavanja": 2,
-    "vjezbe": 2,
-    "index": 12346
-    },
-    {
-    "sedmica": 2,
-    "predavanja": 2,
-    "vjezbe": 0,
-    "index": 12345
-    },
-    {
-    "sedmica": 2,
-    "predavanja": 2,
-    "vjezbe": 0,
-    "index": 12346
-    }
-    ],
-    "predmet": "Razvoj mobilnih aplikacija",
-    "brojPredavanjaSedmicno": 2,
-    "brojVjezbiSedmicno": 2
-};
-
 window.onload = function() {
     let div = document.getElementById("meni");
 
@@ -53,11 +13,8 @@ window.onload = function() {
 
        var lista = document.querySelectorAll("li");
        for(let i = 0; i< lista.length; i++){
-           lista[i].addEventListener("click", function() {
-            //console.log("OK");
+           lista[i].addEventListener("click", function() {+
                PoziviAjax.getPredmet(lista[i].innerHTML, function(error, data) {
-                 //data su podaci o predmeti
-                 //tabela.innerText = data.studenti.ime;
                  TabelaPrisustvo(tabela, data);
                })
            });
@@ -74,19 +31,4 @@ window.onload = function() {
     })
     btnLogout.setAttribute("class", "btnLogout");
     div.appendChild(btnLogout);
-
-       /* 
-    btnLogout.onclick = function () {
-        PoziviAjax.postLogout(function(error, data) {});
-    }
-    */
-    
-
-    /*var lista = "<ul>";
-       for(let i = 0; i < data.length; i++) {
-        lista += "<li>" + data[i] + "</li>";
-       }
-       lista += "</ul>"
-   
-    div.innerHTML = lista;*/
 }
